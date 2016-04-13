84e15ab06eac419dd995dc49928f6f9c21202a90
ktrussell41 <ktrussell41@students.davisonschools.org>
2016-04-13 13:02:18 -0400

    Worked in studios
    Deleted name stuff added itemstorage

--- Final Project text based game/Main.cpp
+++ Final Project text based game/Main.cpp
@@ -7,9 +7,31 @@
 using namespace std;
 
 int main(){
-	string name;
+	string selection;
+	bool game = false;
+	
+	do{
+		cout << "Start game" << endl;
+		cout << "Instructions" << endl;
+		getline(cin,selection);
+		if (selection == "Instructions"){
+			cout << "When making your decision make sure to capitalize the first letter and make sure everything is spelt right." << endl;
+		} 
+		else{
+			game = true;
+		} 
+
+	} while (game = false); 
 
 
+	cout << "Continue to game? Yes or No?" << endl;
+	cin >> selection;
+	if (selection == "No"){
+		system("pause");
+		return 0;
+	}
+
+	
 
 	system("pause");
 	return 0;
