6e5ef3de6b2de99f907a613566083d0254dbbc0f
ktrussell41 <ktrussell41@students.davisonschools.org>
2016-05-05 08:00:17 -0400

    Worked in Visual Studios
    Set kab default in KeyStore.

--- Final Project text based game/KeyStore.cpp
+++ Final Project text based game/KeyStore.cpp
@@ -1,1 +1,8 @@
 #include "KeyStore.h"
+
+void KeyStore::setkabDefault(bool value){
+	kab = value;
+}
+bool KeyStore::getkabDefault(){
+	return kab;
+}
--- Final Project text based game/KeyStore.h
+++ Final Project text based game/KeyStore.h
@@ -3,4 +3,7 @@
 class KeyStore{
 private:
 	bool kab;
+public:
+	void setkabDefault(bool value);
+	bool getkabDefault();
 };
